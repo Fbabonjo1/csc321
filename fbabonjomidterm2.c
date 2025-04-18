dff5f4f8eb9556effea17bf94b58256ad2312881
fbabonjo <fbabonjo1@toromail.csudh.edu>
2025-04-18 01:12:32 +0000

    Midterm 2 question 19 (For me)

---
+++ fbabonjomidterm2.c
@@ -0,0 +1,27 @@
+//Fatoma Babonjo
+//
+//
+//
+#include <stdio.h>
+
+void insertname(int name)
+{
+	for (int i=0;i < name;i++) {
+		printf("My name is Fatoma Babonjo.\n");
+	}
+}
+
+
+
+int main()
+{
+	int number, x = 0;
+	printf("How many times do you want your name to be printed?");
+	scanf("%d", &number);
+
+
+
+
+insertname(number);
+
+}
